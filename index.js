16be39eb03c9c1016cda9db37d20792165ba5bf1
andy <andy@jonezie.com>
2018-02-19 18:41:41 -0500

    addding test mode

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var INFURA_MAINNET_URL = 'https://mainnet.infura.io/gmXEVo5luMPUGPqg6mhy';
 
 
 var https_enabled = process.argv[2] === 'https';
+var test_mode = process.argv[2] === 'test';
 
 
-const poolConfig = require('./pool.config').config;
-const accountConfig = require('./account.config').account;
+
+const poolConfig = require('./pool.config').config
 
 console.log(poolConfig)
 
@@ -15,15 +16,25 @@ console.log('init');
 
 fs = require('fs');
 
+var redisInterface = require('./lib/redis-interface')
 var peerInterface = require('./lib/peer-interface')
 var tokenInterface = require('./lib/token-interface')
 var webServer = require('./lib/web-server')
-
+var accountConfig;
 var Web3 = require('web3')
 
 var web3 = new Web3()
 
-web3.setProvider(INFURA_MAINNET_URL)
+if(test_mode){
+  console.log("Using test mode!!! - Ropsten ")
+  web3.setProvider(INFURA_ROPSTEN_URL)
+
+   accountConfig = require('./test.account.config').account;
+}else{
+  web3.setProvider(INFURA_MAINNET_URL)
+
+   accountConfig = require('./account.config').account;
+}
 
 
 init(web3);
@@ -31,8 +42,10 @@ init(web3);
 
 async function init(web3)
 {
-  await tokenInterface.init(web3,accountConfig,poolConfig)
-  await peerInterface.init(accountConfig,poolConfig,tokenInterface) //initJSONRPCServer();
+  await redisInterface.init()
+
+  await tokenInterface.init(redisInterface,web3,accountConfig,poolConfig,test_mode)
+  await peerInterface.init(accountConfig,poolConfig,redisInterface,tokenInterface,test_mode) //initJSONRPCServer();
 
   webServer.init(https_enabled,peerInterface)
 
