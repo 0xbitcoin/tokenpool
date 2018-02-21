1f7e5b59dde92957ae26fe9e8b4e641895cdf8b7
andy <andy@jonezie.com>
2018-02-20 21:13:16 -0500

    dfferent gas prices

--- pool.config.js
+++ pool.config.js
@@ -1,6 +1,7 @@
 var poolconfig = {
-  minimumShareDifficulty: 1,
-  gasPriceWei: 1,
+  minimumShareDifficulty: 4,
+  solutionGasPriceWei: 5,
+  transferGasPriceWei: 1,
   poolTokenFee: 5,
   minBalanceForTransfer: 100000000
 }
