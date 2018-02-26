a0bf0de673c074bc591a0b34bdf251c123add3ec
Andy <admazzola@gmail.com>
2018-02-26 00:48:04 -0500

    fixing

--- app/assets/javascripts/account-renderer.js
+++ app/assets/javascripts/account-renderer.js
@@ -52,7 +52,7 @@ export default class AccountRenderer {
         for(var i in data)
         {
           var shares = parseInt(data[i].minerData.shareCredits)
-          if( isNaN(shares) || shares < 0)
+          if( isNaN(shares) || shares <= 0)
           {
 
             data.splice(i, 1);
