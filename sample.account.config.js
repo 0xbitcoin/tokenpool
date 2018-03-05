b2892c20f75b23b41c879448e2e7c606b94efd0e
Andy M <admazzola@gmail.com>
2018-03-05 09:52:56 -0500

    altering sample config

--- sample.account.config.js
+++ sample.account.config.js
@@ -1,6 +1,13 @@
-var account = {
-  address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
-  privateKey: '00000'
+var accounts = {
+  mint_account: {
+    address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
+    privateKey: '00000'
+  },
+  transfer_account: {
+    address: '0x877137Df705868fD66f3243bAbF74Ead828ac656',
+    privateKey: '00000'
+  },
+
 }
 
-exports.account = account
+exports.accounts = accounts
