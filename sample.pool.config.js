d4c5d7408d7d3bbd335954311b6ffdf45d1333c8
Andy M <admazzola@gmail.com>
2018-03-16 10:35:45 -0400

    custom web3provider

--- sample.pool.config.js
+++ sample.pool.config.js
@@ -5,21 +5,9 @@ var poolconfig = {
   transferGasPriceWei: 6,
   poolTokenFee: 15,
   minBalanceForTransfer: 1500000000,
-  populationLimit: 100
+  populationLimit: 100,
+  web3provider: 'http://localhost:8545'
 }
 
 
 exports.config = poolconfig;
-
-
-
-
-
-
-
-
-
-
-
-
-
