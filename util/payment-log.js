22d93f335d85540c8ef99513d8f785bc1a7ff752
andy <andy@jonezie.com>
2018-03-28 18:14:15 -0400

    trying to finish script

--- util/payment-log.js
+++ util/payment-log.js
@@ -1,26 +1,88 @@
 
 var fs = require('fs');
 
+var redisInterface = require('../lib/redis-interface')
+
 console.log("Logging Payments...")
 
 
+async function init()
+{
 
-var paymentLog = {};
+   await redisInterface.init();
 
-
-paymentLog.missedPayments = [];
-
-
-paymentLog.missedPayments.push({title:"Hello"})
+  var paymentLog = {};
 
 
+  paymentLog.missedPayments = [];
 
-var paymentLogJSON = JSON.stringify(paymentLog);
 
-fs.writeFile("./logs/payments-log.json", paymentLogJSON, function(err) {
-    if(err) {
-        return console.log(err);
-    }
+  var balancePaymentKeyList = [];
+  var balancePayments = [];
 
-    console.log("The file was saved!");
-});
+
+  await new Promise(function (fulfilled,rejected) {
+
+        redisInterface.getRedisClient().keys('*',  function(err, keys){
+            keys.forEach( function(key,i){
+          //  if(!key.toLowerCase().endsWith('0xf13e2680a930aE3a640188afe0F94aFCeBe7023b'.toLowerCase())) return;
+          //  console.log(key);
+
+            console.log('for each key')
+            if(key.startsWith("balance_payments")){ // get all of the keys that have balance transfer (blockchain transactions)
+              console.log(key);
+              balancePaymentKeyList.push(key);
+            }
+
+          });
+
+            fulfilled();
+        });
+
+
+
+    });
+
+
+
+    console.log("balancePaymentKeyList",balancePaymentKeyList)
+
+  await new Promise(async function (fulfilled,rejected) {
+
+    balancePaymentKeyList.forEach(async function(key,i){
+
+        console.log(key)
+          var balancePayments = await redisInterface.getParsedElementsOfListInRedis(key)
+
+          balancePayments.forEach(function(payment,j){
+            balancePayments.push(payment)
+            console.log(payment)
+          });
+
+      });
+
+      fulfilled()
+
+
+  });
+
+  console.log('balancePayments',balancePayments)
+
+  paymentLog.missedPayments.push({balancePayments: balancePayments })
+
+
+
+  var paymentLogJSON = JSON.stringify(paymentLog);
+
+  fs.writeFile("./logs/payments-log.json", paymentLogJSON, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
+
+}
+
+
+init();
