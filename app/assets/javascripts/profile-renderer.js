1b8bb1b9c74124c640777fcbccbfc2dacb34d46d
mvis <mining.visualizer@gmail.com>
2018-04-10 16:00:20 -0700

    Format hash rate at the top of the profile page

--- app/assets/javascripts/profile-renderer.js
+++ app/assets/javascripts/profile-renderer.js
@@ -72,6 +72,7 @@ export default class ProfileRenderer {
      data.etherscanURL = ('https://etherscan.io/address/'+minerAddress.toString());
 
      data.tokenBalanceFormatted = self.formatTokenQuantity( data.tokenBalance );
+     data.hashRateFormatted = renderUtils.formatHashRate( data.hashRate );
 
      console.log('got miner details')
      console.dir(  data );
