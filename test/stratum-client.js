8f676259b593f7741ef9014f554c6013d101d7ff
mvis <mining.visualizer@gmail.com>
2018-04-28 14:05:05 -0700

    refactor and improvements

--- test/stratum-client.js
+++ test/stratum-client.js
@@ -5,22 +5,21 @@ var HOST = '127.0.0.1';
 var PORT = 5000;
 
 var client = new net.Socket();
+
 client.connect(PORT, HOST, function() {
-
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    client.write('I am Chuck Norris!');
-
+    var msg = {
+    	id : 1,
+	    method : 'subscribe',
+	    params : ['0x1234']
+    };
+    client.write(JSON.stringify(msg));
 });
 
 client.on('data', function(data) {
-    
     console.log('DATA: ' + data);
-    // Close the client socket completely
-    // client.destroy();
-    
 });
 
-// Add a 'close' event handler for the client socket
 client.on('close', function() {
     console.log('Connection closed');
 });
