fea8842106f46417aa2a6c8786528cd4d2b3318a
Andy <admazzola@gmail.com>
2018-04-29 14:50:24 -0400

    autoxfer

--- sample.pool.config.js
+++ sample.pool.config.js
@@ -5,6 +5,7 @@ var poolconfig = {
   transferGasPriceWei: 6,
   poolTokenFee: 15,
   minBalanceForTransfer: 1500000000,
+  payoutWalletMinimum: 1000,
   populationLimit: 100,
   web3provider: "http://127.0.0.1:8545"
 }
