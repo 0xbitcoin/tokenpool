8a3100bbe36c6f353d037d6c7d2fe0a6b248104b
mvis <mining.visualizer@gmail.com>
2018-04-28 21:37:56 -0700

    handle share submission

--- test/stratum-client.js
+++ test/stratum-client.js
@@ -6,20 +6,46 @@ var PORT = 5000;
 
 var client = new net.Socket();
 
-client.connect(PORT, HOST, function() {
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    var msg = {
-    	id : 1,
-	    method : 'mining.subscribe',
-	    params : ['0x1234']
-    };
-    client.write(JSON.stringify(msg) + '\n');
+
+client.connect(PORT, HOST);
+
+client.on('connect', function() {
+   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+
+   // subscribe to work notifications
+   var msg = {
+      id : 1,
+      method : 'mining.subscribe',
+      params : ['0x_miner_eth_address']
+   };
+   client.write(JSON.stringify(msg) + '\n');
+
+   // send some shares to the pool every so often
+   setInterval(submitShare, 10 * 1000);
+
+}).on('data', function(data) {
+   // listen for :
+   //    - mining.notify messages 
+   //    - server responses to our subscribe msg and share submits
+   console.log('DATA: ' + data);
+   
+}).on('close', function() {
+   console.log('Connection closed');
 });
 
-client.on('data', function(data) {
-    console.log('DATA: ' + data);
-});
 
-client.on('close', function() {
-    console.log('Connection closed');
-});
+function submitShare() {
+   console.log('submitting share');
+   var msg = {
+      id : 4,
+      method : 'mining.submit',
+      params : [
+         '0x_nonce',
+         '0x_miner_eth_address',
+         '0x_digest',
+         '123_difficulty',
+         '0x_challenge'
+         ]
+   };
+   client.write(JSON.stringify(msg) + '\n');
+}
