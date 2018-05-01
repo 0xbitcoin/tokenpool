127a86327d9b7399ba2194e1698430c88cf4ac9d
mvis <mining.visualizer@gmail.com>
2018-05-01 15:06:54 -0700

    stratum-server receives varDiff changes

--- stratum.js
+++ stratum.js
@@ -1,12 +1,49 @@
 
 
+var INFURA_ROPSTEN_URL = 'https://ropsten.infura.io/gmXEVo5luMPUGPqg6mhy';
+var INFURA_MAINNET_URL = 'https://mainnet.infura.io/gmXEVo5luMPUGPqg6mhy';
+
 var redisInterface = require('./lib/redis-interface')
 var stratumServer = require('./lib/stratum-server')
+
+var accountConfig;
+
+var pool_env = 'production';
+
+if( process.argv[2] == "test" )
+{
+  pool_env = 'test'
+}
+
+if( process.argv[2] == "staging" )
+{
+  pool_env = 'staging'
+}
+
+const poolConfig = require('./pool.config').config
+
+var specified_web3 = poolConfig.web3provider;
+
+if (pool_env == "test") {
+   console.log("Using test mode!!! - Ropsten ")
+   specified_web3 = specified_web3 || INFURA_ROPSTEN_URL;
+   accountConfig = require('./test.account.config').accounts;
+} else if (pool_env == "staging") {
+    console.log("Using staging mode!!! - Mainnet ")
+    specified_web3 = specified_web3 || INFURA_MAINNET_URL;
+   accountConfig = require('./account.config').accounts;
+} else {
+    specified_web3 = specified_web3 || INFURA_MAINNET_URL;
+    accountConfig = require('./account.config').accounts;
+}
+
+// web3.setProvider(specified_web3)
+// console.log('using web3', specified_web3)
 
 init();
 
 async function init()
 {
    await redisInterface.init()
-   await stratumServer.init();
+   await stratumServer.init(accountConfig);
 }
--- test/miner-client.js
+++ test/miner-client.js
@@ -2,6 +2,17 @@
 const net = require('net');
 const os = require('os');
 
+var miners = ['0x3650cb4be89370dc547205138bb1d6533eb20ec2', '0x1b7bfB694eE51913c347971c7090a74AEFbd41f6'];
+
+if (process.argv[2] == null) {
+   console.log('please specify a miner # on the command line');
+   return;
+}
+
+console.log('argv[2]=', process.argv[2]);
+
+var minerAccount = miners[process.argv[2]];
+
 var HOST = '127.0.0.1';
 var PORT = 5000;
 
@@ -11,17 +22,18 @@ client.connect(PORT, HOST);
 
 client.on('connect', function() {
    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+   console.log('Local port:', client.localPort);
 
    // subscribe to work notifications
    var msg = {
       id : 1,
       method : 'mining.subscribe',
-      params : ['0x_miner_eth_address']
+      params : [minerAccount]
    };
    client.write(JSON.stringify(msg) + '\n');
 
    // send some shares to the pool every so often
-   setInterval(submitShare, 10 * 1000);
+   // setInterval(submitShare, 10 * 1000);
 
 }).on('data', function(data) {
    // listen for :
