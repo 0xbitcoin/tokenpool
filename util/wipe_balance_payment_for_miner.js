71318dcfc8ab9cba45663edd81f0a39850f4da87
Andy M <admazzola@gmail.com>
2018-06-12 14:31:07 -0400

    script to remove miner pending balance

---
+++ util/wipe_balance_payment_for_miner.js
@@ -0,0 +1,61 @@
+
+
+
+var redisInterface = require('../lib/redis-interface')
+
+
+init();
+
+
+async function init()
+{
+
+
+   var minerAddress = process.argv[2]
+
+   console.log('minerAddress',minerAddress)
+
+   var redis = await redisInterface.init()
+
+
+   //set miners pending balance to 0
+
+   var minerData = await getMinerData(minerAddress)
+
+   if(minerData ==null)
+   {
+     console.log('no miner data exists!', minerAddress  )
+     return;
+   }
+
+   minerData.tokenBalance = 0;
+
+   await  saveMinerDataToRedis(minerAddress,minerData)
+
+
+  // var balance_xfers = await redisInterface.deleteHashArrayInRedis('balance_payment')
+
+   console.log('saved miner data!', minerAddress, minerData )
+   return;
+}
+
+
+async function getMinerData(minerEthAddress)
+{
+
+  var minerDataJSON = await  redisInterface.findHashInRedis("miner_data", minerEthAddress );
+
+  if(minerDataJSON==null)
+  {
+      return null;
+  }
+
+  return JSON.parse(minerDataJSON) ;
+
+}
+
+async function saveMinerDataToRedis(minerEthAddress, minerData)
+{
+  await this.redisInterface.storeRedisHashData("miner_data", minerEthAddress , JSON.stringify(minerData))
+
+}
