983e3127f237f0bf131a321804c22db0903866f6
andy <admazzola@gmail.com>
2021-03-03 18:47:19 -0500

    mocha

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -5,24 +5,23 @@ const Web3 = require('web3');
 
 export default class TestHelper{
 
-    static async deployContract( compiledContractJSON ){
+    static async deployContract(web3,  compiledContractJSON ){
         
-            const provider = ganache.provider();
-            provider.setMaxListeners(15);       // Suppress MaxListenersExceededWarning warning
-            const web3 = new Web3(provider);
-            this.accounts = await web3.eth.getAccounts();
-        
+            let accounts = await web3.eth.getAccounts();
+
             // Read in the compiled contract code and fetch ABI description and the bytecode as objects
             const compiled = compiledContractJSON // JSON.parse(fs.readFileSync("output/contracts.json"));
             const abi = compiled.abi;
             const bytecode = compiled.bytecode;
         
             // Deploy the contract and send it gas to run.
-            this.contract = await new web3.eth.Contract(abi)
+            let contract = await new web3.eth.Contract(abi)
                 .deploy({data:  bytecode, arguments: []})
-                .send({from: this.accounts[0], gas:'5000000'});
-        
-            return this;
+                .send({from: accounts[0], gas:'5000000'});
+
+            
+
+            return {contract: contract, accounts: accounts};
        
 
 
--- test/mocha.js
+++ test/mocha.js
@@ -10,11 +10,38 @@ import TestHelper from './TestHelper'
 
 var assert = chai.assert;
 
+var tokenContract 
+
 describe('Pool System', async function() {
   it('should deploy contract', async  function() {
 
+
+    const provider = ganache.provider();
+    provider.setMaxListeners(15);       // Suppress MaxListenersExceededWarning warning
+    const web3 = new Web3(provider);
+    let accounts = await web3.eth.getAccounts();
+
+
+
   //  let compiledContractJSON = TokenContractJSON
-    let contract = await TestHelper.deployContract( TokenContractJSON  )
+    let deployment = await TestHelper.deployContract(web3, TokenContractJSON  )
+    console.log('deployed! ', deployment)
+
+
+    tokenContract = deployment.contract 
+
+     
+
+
+  });
+
+
+  it('should read challenge number from contract ', async  function() {
+
+    let challengeNumber = await tokenContract.methods.challengeNumber().call()
+    
+    assert.isOk( challengeNumber );
+     
 
   });
 });
